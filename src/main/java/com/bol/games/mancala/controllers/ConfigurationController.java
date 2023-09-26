9b3d8d97323ed33e3c8a4027e3f147372aeddd3b
Erick T <erick_turcios@hotmail.com>
2023-09-26 03:33:47 -0600

    Remove unused imports

--- src/main/java/com/bol/games/mancala/controllers/ConfigurationController.java
+++ src/main/java/com/bol/games/mancala/controllers/ConfigurationController.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
